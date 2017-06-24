61da78f3e311e6d3db1bd64bb8c657944abd3101
Ivan Cui <ivancjr@uw.edu>
2017-06-23 18:15:01 -0700

    finished work

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+
+f = list(csv.reader(open('faculty.csv', 'r')))
+
+email_list = []
+for email in f:
+    email_list.append(email[3].strip())
+
+email_list = email_list[1:]
+    
+with open('email_list_output.csv', 'w') as f:
+    writer = csv.writer(f, delimiter = ',',  lineterminator = '\r\n')
+    for email in email_list:
+        writer.writerow([email])
+                         
